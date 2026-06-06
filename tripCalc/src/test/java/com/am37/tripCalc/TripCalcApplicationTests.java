03d978ebed63fae4f41441f6c5029d3068b4e80b
AmelRidzal~ <amelridzal37@gmail.com~>
2026-06-06 14:13:39 +0200

    init

---
+++ tripCalc/src/test/java/com/am37/tripCalc/TripCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am37.tripCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
